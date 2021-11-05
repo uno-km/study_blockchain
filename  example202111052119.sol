f6f54cbac61b1fa241fdd7152858cf9d33776962
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-05 21:21:16 +0900

    솔리디티 연습

---
+++  example202111052119.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.10;
+
+contract test {
+
+    function arith1() public pure returns (uint) {
+        uint    a = 3;
+        uint    b = 2;
+        return a * b;
+    }
+}
+
+
