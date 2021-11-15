7ba17d9af0ae270dd334c5ebe0ff63e10b7065f1
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-15 20:35:47 +0900

    메모리에 저장하기 위한 방법 공부

---
+++ Array/Storage3.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.10;
+
+contract Storage3 {
+    uint []a; // 스토리지에 저장
+    uint []b= new uint[](2); // 메모리에 저장
+
+    function ex1(uint len) public pure {
+        uint []memory d; // 메모리 저장
+        uint []memory e = new uint[](10); // 메모리에 저장
+        e[1] = 10;
+    }
+}
+
+
