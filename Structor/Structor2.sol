145d59d137526171e5fbb293b6581ad85001ab4a
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-22 21:45:49 +0900

    구조체 선언

---
+++ Structor/Structor2.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.10;
+
+contract Structor2 {
+
+    struct St {
+        uint no;
+        string name;
+    }
+    St[] ss1;
+    St[] public ss2;
+}
+
+
