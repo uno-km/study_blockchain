ff2a74366db1fa1a105cde33ec6a6e6397bde26d
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-22 21:43:20 +0900

    구조체 실습

---
+++ Structor/Struct.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.10;
+
+contract Struct {
+
+    struct St {
+        uint no;
+        string name;
+    }
+    St kim = St(10101, "kim");
+
+    St[] stClass1.push(St(20,"kim"));
+
+}
+
+
