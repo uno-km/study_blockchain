37afe082fb267fe057be59fb9db5d630e01146eb
Remix Workspace <>
2022-08-09 00:54:44 +0900

    2강 수업완료/address 타입 공부완료

--- Dev_in_REMIX/lec2.sol
+++ Dev_in_REMIX/lec2.sol
@@ -19,9 +19,16 @@ contract lec2{
     // bytes :  
     bytes4 public bt = 0x12345678;
     bytes public bt2 = "STRING";
+    bytes public bt3 = "HiMynameisUnokim.";
+    //어드레스 : 다른언어에 없는 타입이다. 
+    // 은행계좌를 개설하면 고유 계좌인데 마찬가지로 그런 계좌번호라고 생각하면된다.
+    // 이더를 송수신할때 이 어드레스를 통해서 주고받고  
+    // 어드레스는 사람에게만 부여되는게 아니라. 스맡컨트랙을 배포할 때도 어드레스가 발생한다. 
+    // 즉 스마트 컨트랙 마다 배포가 되면은 어드레스가 생간다. 
     //address :  0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47
+    //0xd9145CCE52D386f254917e481eB44e9943F39138
     address public addr = 0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47;
-    // int vs uint  : -가 있냐 없냐
+    // int vs uint  : -가 있냐 없냐의 차이다. 
     // int8 : -2^7 ~ 2^7-1
     // uint8 : 0~2^8-1 
     int8 public  it =4;
